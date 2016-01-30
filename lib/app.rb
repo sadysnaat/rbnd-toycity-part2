bc27ade6905f0f3eb847e73b8f442cb457a0b1b6
Deepak Sharma <sadysnaat@gmail.com>
2016-01-30 22:24:11 +0530

    add ascii art and todays date

--- lib/app.rb
+++ lib/app.rb
@@ -3,11 +3,29 @@ path = File.join(File.dirname(__FILE__), '../data/products.json')
 file = File.read(path)
 products_hash = JSON.parse(file)
 
+time = Time.new()
 # Print "Sales Report" in ascii art
+puts "  _____       _             _____                       _"
+puts " / ____|     | |           |  __ \\                     | |"
+puts "| (___   __ _| | ___  ___  | |__) |___ _ __   ___  _ __| |_"
+puts " \\___ \\ / _` | |/ _ \\/ __| |  _  // _ \\ '_ \\ / _ \\| '__| __|"
+puts " ____) | (_| | |  __/\\__ \\ | | \\ \\  __/ |_) | (_) | |  | |_"
+puts "|_____/ \\__,_|_|\\___||___/ |_|  \\_\\___| .__/ \\___/|_|   \\__|"
+puts "                                      | |"
+puts "                                      |_|"
 
 # Print today's date
+puts time.strftime("%Y-%m-%d %H:%M:%S")
 
 # Print "Products" in ascii art
+puts "                     _            _       "
+puts "                    | |          | |      "
+puts " _ __  _ __ ___   __| |_   _  ___| |_ ___ "
+puts "| '_ \\| '__/ _ \\ / _` | | | |/ __| __/ __|"
+puts "| |_) | | | (_) | (_| | |_| | (__| |_\\__ \\"
+puts "| .__/|_|  \\___/ \\__,_|\\__,_|\\___|\\__|___/"
+puts "| |                                       "
+puts "|_|                                       "
 
 # For each product in the data set:
 	# Print the name of the toy
@@ -18,6 +36,13 @@ products_hash = JSON.parse(file)
 	# Calculate and print the average discount (% or $) based off the average sales price
 
 # Print "Brands" in ascii art
+puts " _                         _     "
+puts "| |                       | |    "
+puts "| |__  _ __ __ _ _ __   __| |___ "
+puts "| '_ \\| '__/ _` | '_ \\ / _` / __|"
+puts "| |_) | | | (_| | | | | (_| \\__ \\"
+puts "|_.__/|_|  \\__,_|_| |_|\\__,_|___/"
+puts
 
 # For each brand in the data set:
 	# Print the name of the brand
